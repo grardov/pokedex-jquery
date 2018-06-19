03afb46775fa600145e179287d2800b848f48589
Gerardo <grdvalencia.r@gmail.com>
2018-06-19 17:41:30 -0500

    Refactor sidebar script

---
+++ assets/js/sidebar.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("sidebar").style.left = "0";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("sidebar").style.left = "-300px";
+  document.body.style.backgroundColor = "white";
+}
